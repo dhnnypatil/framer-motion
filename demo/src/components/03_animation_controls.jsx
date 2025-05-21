c260d355907f450fe373eac44fb98c83e0392503
Dhanay Patil <btech10581.23@bitmesra.ac.in>
2025-05-21 16:34:03 +0530

    some basics
    motion.anything for the motion elements and initial, animate, transition, exit, style, variants being the major attributes.

---
+++ demo/src/components/03_animation_controls.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState } from "react";
+import {motion, useAnimationControls} from "motion/react";
+
+const AnimationControls = () =>{
+    const controls = useAnimationControls();
+
+    const handleClick = () => {
+        controls.start("flip");
+    }
+
+    return(
+        <div
+            style={{
+                display: "grid",
+                placeContent: "center",
+                height: "100vh",
+                gap: "0.8rem",
+            }}>
+                <button 
+                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                    onClick={handleClick}>
+                        Flip
+                </button>
+
+                <motion.div 
+                    style = {{
+                        width: "100px",
+                        height: "100px",
+                        backgroundColor: "yellow",
+                    }}
+                    variants={{
+                        initial:{
+                            rotate: 0,
+                        },
+                        flip:{
+                            rotate: 360,
+                        }
+                    }}
+                    initial="initial"
+                    animate={controls}
+                >                               
+                </motion.div>  
+        </div>
+    )
+}
+
+export default AnimationControls;
---
+++ demo/src/components/04_viewbased_animations.jsx
@@ -0,0 +1,46 @@
+import React, { useRef, useEffect } from "react";
+import { motion, useInView } from "motion/react"; 
+
+const ViewBasedAnimations = () => {
+    const ref = useRef(null);
+    const isInView = useInView(ref);
+
+    useEffect(() => {
+        console.log("is in View: ", isInView);
+    }, [isInView]);
+
+    return(
+        <>
+            <div 
+                style={{
+                    height: "150vh",
+                }}
+            ></div>
+            <motion.div
+                style={{
+                    height: "100vh",
+                    backgroundColor: "black",
+                }}
+                initial={{ 
+                    opacity: 0,
+                }}
+                whileInView={{ 
+                    opacity: 1,
+                }}
+                transition={{
+                    duration: 1,
+                }}
+            ></motion.div>     
+            <div
+                ref={ref}
+                style={{
+                    height: "100vh",
+                    backgroundColor: isInView ? "black" : "grey",
+                    transition: "2.2s background-color",
+                }}
+            ></div>
+        </>
+    );
+};
+
+export default ViewBasedAnimations;
