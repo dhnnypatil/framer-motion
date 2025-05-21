e4ef223e570afc713b02638c40ebe14ca0a60aec
Dhanay Patil <btech10581.23@bitmesra.ac.in>
2025-05-22 03:43:20 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
   plugins: [react()],
   // Using relative path for GitHub Pages compatibility
   base: '/hirebuddy_landing-page',
-  build: {
-    // Ensure source maps are generated for easier debugging
-    sourcemap: true,
-    // Output more information during build
-    reportCompressedSize: true,
-    // Make sure assets are correctly processed
-    assetsInlineLimit: 4096
-  }
+  // build: {
+  //   // Ensure source maps are generated for easier debugging
+  //   sourcemap: true,
+  //   // Output more information during build
+  //   reportCompressedSize: true,
+  //   // Make sure assets are correctly processed
+  //   assetsInlineLimit: 4096
+  // }
 })
