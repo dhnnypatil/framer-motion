6fdee9b2036d92f6a7ee62e30bc3f02a50a5fcac
Dhanay Patil <btech10581.23@bitmesra.ac.in>
2025-05-22 02:44:44 +0530

    updated .yml (again)

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Using relative path for GitHub Pages compatibility
-  base: './',
+  base: '/hirebuddy_landing-page',
   build: {
     // Ensure source maps are generated for easier debugging
     sourcemap: true,
